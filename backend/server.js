f9b0234655b5845c2862526baffe7beb6ccef7c0
Jahongir <hamidovjahongir444@gmail.com>
2021-03-29 21:07:02 +0500

    prepare deploying

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,21 @@ app.use('/api/upload', uploadRoutes)
 
 // app.get('/api/config/click', (req,res) => res.send() )
 
+const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req,res) => {
+        res.send('API is running ...')
+    })
+}
+
+
 
 // //Static file
 // app.use('/images',express.static('images'))
